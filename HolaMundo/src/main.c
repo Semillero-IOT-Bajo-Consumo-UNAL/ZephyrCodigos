03a666d79b439339cb085f0fedea84b9c2a0f7be
Ash <savalderrama@unal.educ.o>
2025-04-13 10:30:44 -0500

    Initial Commit

---
+++ HolaMundo/src/main.c
@@ -0,0 +1,20 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+
+/* Definimos la constante SLEEP_TIME_MS como 1000 representando 1000 milisegundos  */
+#define SLEEP_TIME_MS   1000
+
+int main(void)
+{
+    // Imprimimos un mensaje
+	printk("ejemplo: Nuestro programa ha iniciado xd \n");
+    // Imprimimos un mensaje, esperamos 1 segundo y repetimos el codigo
+	while (1) {
+		printk("ejemplo: esto se deberia ejecutar cada segundo \n");
+		k_msleep(SLEEP_TIME_MS);
+	}
+	// Retornamos 0 (el numero que pasemos indica codigos de error)
+	// al ser 0 estamos diciendo que el programa finalizo correctamente
+	return 0;
+}
