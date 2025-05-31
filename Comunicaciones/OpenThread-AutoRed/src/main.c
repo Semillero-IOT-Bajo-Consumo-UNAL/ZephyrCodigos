c6d85408b8c4f564086c462499a4acdb511f437b
Ash <savalderrama@unal.edu.co>
2025-05-31 18:27:26 -0500

    test

--- Comunicaciones/OpenThread-AutoRed/src/main.c
+++ Comunicaciones/OpenThread-AutoRed/src/main.c
@@ -1,5 +1,7 @@
+#include <openthread/instance.h>
+#include <zephyr/net/openthread.h>
 
-
-void main(void)
-{
+void main(void) {
+    struct openthread_context *ot_context = openthread_get_default_context();
+    openthread_start(ot_context);
 }
